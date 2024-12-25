ec9c0ccda508f9b30a4e166fa14429930e5bef6a
Prototype720 <huzouskaya.aliaksandra@gmail.com>
2024-12-25 10:11:59 +1000

    shims-vue.d.ts and trying in diary

--- diary/django_diary_client/src/main.ts
+++ diary/django_diary_client/src/main.ts
@@ -5,8 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 const app = createApp(App)
+
+app.config.globalProperties.$http = axios;
 
 app.use(createPinia())
 app.use(router)
