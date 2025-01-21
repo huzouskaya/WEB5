fee5545d767ee193ca0053b865a078cad0223e7f
Prototype720 <huzouskaya.aliaksandra@gmail.com>
2025-01-22 00:05:55 +1000

    The greatest initialization in the world

--- diary/notes/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers, serializers, viewsets
-from .models import DiaryEntry
-
-class DiarySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = DiaryEntry
-        fields = ['id', 'title', 'content', 'created_at', 'updated_at']
